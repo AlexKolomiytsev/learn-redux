acac23b620174b98e7837cca185594c542dfbcb7
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2018-05-07 23:21:30 +0300

    withRouter

--- src/learning/reactTodoList/components/VisibileTodoLIst.tsx
+++ src/learning/reactTodoList/components/VisibileTodoLIst.tsx
@@ -4,18 +4,19 @@ import { connect } from 'react-redux';
 import { TOGGLE_TODO_ACTION } from '../actions';
 import { getVisibleTodos } from '../helpers';
 import { TodoList } from './TodoList';
+import {withRouter} from 'react-router';
 
-const mapStateToProps = (state) => ({
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+const mapStateToProps = (state, { match: { params } }) => ({
+    todos: getVisibleTodos(state.todos, params.filter || 'all')
 });
 
 const mapDispatchToProps = (dispatch) => ({
     onClickTodo: (id) => dispatch(TOGGLE_TODO_ACTION({ id }))
 });
 
-const VisibleTodoList = connect(
+const VisibleTodoList = withRouter(connect(
     mapStateToProps,
     mapDispatchToProps
-)(TodoList);
+)(TodoList));
 
 export default VisibleTodoList;
--- src/learning/reactTodoList/helpers.ts
+++ src/learning/reactTodoList/helpers.ts
@@ -2,11 +2,11 @@ import { isNullOrUndefined } from 'util';
 
 export const getVisibleTodos = (todos, filter) => {
     switch (filter) {
-        case 'SHOW_ALL':
+        case 'all':
             return todos;
-        case 'SHOW_COMPLETED':
+        case 'completed':
             return todos.filter((todo) => todo.completed);
-        case 'SHOW_ACTIVE':
+        case 'active':
             return todos.filter((todo) => !todo.completed);
         default:
             return todos;
